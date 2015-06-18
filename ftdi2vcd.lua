8fc1583c0bbc6ce939bc71354f588b7b9ecbb693
Simon Schubert <2@0x2c.org>
2015-06-18 23:29:19 +0200

    add readme & license

--- ftdi2vcd.lua
+++ ftdi2vcd.lua
@@ -1,3 +1,22 @@
+-- ftdi2vcd - wireshark plugin to convert FTDI MPSSE captures to VCD signal dumps
+--
+-- Copyright (c) 2015 Simon Schubert <2@0x2c.org>.
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU Affero General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU Affero General Public License for more details.
+--
+--    You should have received a copy of the GNU Affero General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 local usb_transfer_type_f = Field.new("usb.transfer_type")
 local usb_direction_f = Field.new("usb.endpoint_number.direction")
 local usb_capdata_f = Field.new("usb.capdata")
