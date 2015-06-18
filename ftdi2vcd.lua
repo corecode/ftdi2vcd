3c30e3742ea348550c4008ac447a835075c5c542
Simon Schubert <2@0x2c.org>
2015-06-18 23:15:03 +0200

    output signals

--- ftdi2vcd.lua
+++ ftdi2vcd.lua
@@ -78,114 +78,146 @@ end
 FTDI = {}
 FTDI.__index = FTDI
 
+FTDI.pinmap = { CLK = 0, TDI = 1, TDO = 2, TMS = 3 }
+
 function FTDI.new(outdata, indata)
    local self = setmetatable({}, FTDI)
    self.o = BBuf.new(outdata)
    self.i = BBuf.new(indata)
+   self.time = 0
+   self.pinstate = { "X", "X", "X", "X", "X", "X", "X", "X",
+                     "X", "X", "X", "X", "X", "X", "X", "X" }
    return self
 end
 
 function FTDI:cmd_19()
    local len = self.o:read_uint16() + 1
-   print("data out bytes", len)
-   self.o:skip(len)
+   self:comment("data out bytes", len)
+   for i = 1, len do
+      local val = self.o:read_uint8()
+      self:clock(8, {TDI = val})
+   end
 end
 
 function FTDI:cmd_1b()
    local len = self.o:read_uint8()
    local outval = self.o:read_uint8()
-   print("data out bits", len, "outval", outval)
+   self:comment("data out bits", len, "outval", outval)
+   self:clock(len, {TDI = outval})
 end
 
 function FTDI:cmd_28()
    local len = self.o:read_uint16() + 1
-   print("data in bytes", len)
-   self.i:skip(len)
+   self:comment("data in bytes", len)
+   for i = 1, len do
+      local val = self.i:read_uint8()
+      self:clock(8, {TDO = val})
+   end
 end
 
 function FTDI:cmd_2a()
    local len = self.o:read_uint8() + 1
    local inval = self.i:read_uint8()
-   print("data in bits", len, "inval", inval)
+   self:comment("data in bits", len, "inval", inval)
+   self:clock(len, {TDO = inval})
 end
 
 function FTDI:cmd_39()
    local len = self.o:read_uint16() + 1
-   print("data in out bytes", len)
-   self.o:skip(len)
-   self.i:skip(len)
+   self:comment("data in out bytes", len)
+   for i = 1, len do
+      local outval = self.o:read_uint8()
+      local inval = self.i:read_uint8()
+      self:clock(8, {TDI = outval, TDO = inval})
+   end
 end
 
 function FTDI:cmd_3b()
    local len = self.o:read_uint8() + 1
    local outval = self.o:read_uint8()
    local inval = self.i:read_uint8()
-   print("data in out bits", len, "outval", outval, "inval", inval)
+   self:comment("data in out bits", len, "outval", outval, "inval", inval)
+   self:clock(len, {TDI = outval, TDO = inval})
 end
 
 function FTDI:cmd_4b()
    local len = self.o:read_uint8() + 1
    local val = self.o:read_uint8()
-   print("tms data out len", len, "val", val)
+   self:comment("tms data out len", len, "val", val)
+   self:set("TDI", bit32.extract(val, 7))
+   self:clock(len, {TMS = val})
 end
 
 function FTDI:cmd_6b()
    local len = self.o:read_uint8() + 1
    local outval = self.o:read_uint8()
    local inval = self.i:read_uint8()
-   print("tms data inout len", len, "outval", outval, "inval", inval)
+   self:comment("tms data inout len", len, "outval", outval, "inval", inval)
+   self:set("TDI", bit32.extract(outval, 7))
+   self:clock(len, {TMS = outval, TDO = inval})
 end
 
 function FTDI:cmd_80()
    local val = self.o:read_uint8()
    local dir = self.o:read_uint8()
-   print("set lo", val, dir)
+   self:setpins(0, val, dir)
 end
 
 function FTDI:cmd_82()
    local val = self.o:read_uint8()
    local dir = self.o:read_uint8()
-   print("set hi", val, dir)
+   self:setpins(1, val, dir)
 end
 
 function FTDI:cmd_85()
-   print("disconnect loopback")
+   -- print("disconnect loopback")
 end
 
 function FTDI:cmd_86()
    local div = self.o:read_uint16()
-   print("set tck divisor", div)
+   self:comment("set tck divisor", div)
 end
 
 function FTDI:cmd_87()
-   print("flush")
+   -- self:comment("flush")
 end
 
 function FTDI:cmd_8a()
-   print("disable clk divide")
+   self:comment("disable clk divide")
 end
 
 function FTDI:cmd_8d()
-   print("disable 3 phase data clocking")
+   -- self:comment("disable 3 phase data clocking")
 end
 
 function FTDI:cmd_97()
-   print("disable adaptive clocking")
+   -- self:comment("disable adaptive clocking")
 end
 
 function FTDI:cmd_unknown(opb)
    if self.i:peek_uint8() == 0xfa then
       self.i:read_uint8()
-      print(string.format("invalid op %02x", opb))
+      self:comment(string.format("invalid op %02x", opb))
       if self.i:read_uint8() ~= opb then
          print("no error from ftdi")
       end
    else
-      print(string.format("unknown op %02x", opb))
+      self:comment(string.format("unknown op %02x", opb))
    end
 end
 
 function FTDI:process()
+   for i = 0, 15 do
+      local name = i
+      for k, v in pairs(self.pinmap) do
+         if v == i then
+            name = k
+         end
+      end
+      print(("$var wire 1 %d %s $end"):format(i, name))
+   end
+   print("$enddefinitions $end")
+
    while not self.o:eof() do
       local opb = self.o:read_uint8()
 
@@ -194,10 +226,55 @@ function FTDI:process()
          fun = self["cmd_unknown"]
       end
 
-      if not pcall(fun, self, opb) then
-         print(("error processing op %02x"):format(opb))
-         print("outpos", self.o.pos, self.o.value:len())
-         print("inpos", self.i.pos, self.i.value:len())
+      -- if not pcall(fun, self, opb) then
+      --    print(("error processing op %02x"):format(opb))
+      --    print("outpos", self.o.pos, self.o.value:len())
+      --    print("inpos", self.i.pos, self.i.value:len())
+      -- end
+      fun(self, opb)
+   end
+end
+
+function FTDI:set(sig, val)
+   if self.pinmap[sig] then
+      sig = self.pinmap[sig]
+   end
+
+   if self.pinstate[sig + 1] == val then
+      return
+   end
+
+   self.pinstate[sig + 1] = val
+
+   print(("%d%s"):format(val, sig))
+end
+
+function FTDI:time_passes()
+   self.time = self.time + 5
+   print(("#%d"):format(self.time))
+end
+
+function FTDI:clock(len, sigs)
+   for i = 0, len - 1 do
+      for k, v in pairs(sigs) do
+         self:set(k, bit32.extract(v, i))
+      end
+      self:set("CLK", 0)
+      self:time_passes()
+      self:set("CLK", 1)
+      self:time_passes()
+   end
+end
+
+function FTDI:setpins(hi, val, mask)
+   for i = 0, 7 do
+      if bit32.extract(mask, i) == 1 then
+         self:set(i + hi * 8, bit32.extract(val, i))
       end
    end
+end
+
+function FTDI:comment(...)
+   local str = table.concat({...},"\t")
+   print(("$comment %s $end"):format(str))
 end
